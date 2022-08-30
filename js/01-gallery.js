5ca80df9246f183960665824bea154b64f9c5902
YevheniiBiloshapka <106679993+YevheniiBiloshapka@users.noreply.github.com>
2022-08-31 00:08:43 +0300

    finished my homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -9,47 +9,41 @@ import { galleryItems } from "./gallery-items.js";
 * 3. Подключение скрипта и стилей библиотеки модального окна basicLightbox. Используй CDN сервис jsdelivr 
 * и добавь в проект ссылки на минифицированные (.min) файлы библиотеки.
 * 4. Открытие модального окна по клику на элементе галереи. Для этого ознакомься с документацией и примерами.
-5. Замена значения атрибута src элемента <img> в модальном окне перед открытием. 
-6. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox. */
-
-// const { preview, original, description } = galleryItems;
+* 5. Замена значения атрибута src элемента <img> в модальном окне перед открытием. 
+*6. Используй готовую разметку модального окна с изображением из примеров библиотеки basicLightbox. */
 
 const gallery = document.querySelector(".gallery");
+gallery.addEventListener("click", openModalWindow);
+
 const cardImg = createImgCard(galleryItems);
 
 gallery.insertAdjacentHTML("beforeend", cardImg);
-gallery.addEventListener("click", galleryImageClick);
-
 function createImgCard(items) {
   return items
-    .map(({ preview, description }) => {
-      return `<img
-        class="gallery__image"
-		
-        src="${preview}"
-        alt="${description}"
-        width="320px"
-      />`;
+    .map(({ preview, description, original }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
     })
     .join("");
 }
 
-function galleryImageClick(event) {
+function openModalWindow(event) {
+  event.preventDefault();
   const imageEl = event.target.classList.contains("gallery__image");
   if (!imageEl) {
     return;
   }
-  // ! -------------------------------------
-  createModalWindow(event.target);
-}
-
-function createModalWindow(elementName) {
-  const newElement = elementName;
-  newElement.classList.remove("gallery__image");
-  newElement.classList.add("gallery__link");
-  newElement.src = `${original}`;
-
-  basicLightbox.create(newElement).show();
-  //   `<img width="1400" height="900" src="https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg">`
-  console.log(newElement);
+  basicLightbox
+    .create(
+      `<img src = "${event.target.dataset.source}" alt="${event.target.alt}" width="800" height="600" />`
+    )
+    .show();
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+/* Выполняй это задание в файлах 02-lightbox.html и 02-lightbox.js. 
+Разбей его на несколько подзадач:
 
-console.log(galleryItems);
+* 1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента 
+*галереи. Используй готовый код из первого задания.
+* 2. Подключение скрипта и стилей библиотеки используя CDN сервис cdnjs. Необходимо добавить 
+*ссылки на два файла: simple-lightbox.min.js и simple-lightbox.min.css.
+* 3. Инициализация библиотеки после того как элементы галереи созданы и добавлены в div.gallery. 
+* Для этого ознакомься с документацией SimpleLightbox - в первую очередь секции «Usage» и «Markup».
+* 4. Посмотри в документации секцию «Options» и добавь отображение подписей к изображениям 
+* из атрибута alt. Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия изображения. */
+
+const galleryBox = document.querySelector(".gallery");
+
+const cardImg = createImgCard(galleryItems);
+galleryBox.insertAdjacentHTML("beforeend", cardImg);
+
+function createImgCard(items) {
+  return items
+    .map(({ preview, description, original }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"  />
+</a>`;
+    })
+    .join("");
+}
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionsData: `alt`,
+  captionDelay: 250,
+});
